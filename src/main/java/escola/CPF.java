c297a40608d245dde40add47ab7a2e69a942acdf
Thiago Machado <56802688+thiagomachadoo@users.noreply.github.com>
2022-01-23 20:07:22 -0300

    regras do cpf

--- src/main/java/escola/CPF.java
+++ src/main/java/escola/CPF.java
@@ -1,4 +1,13 @@
 package escola;
 
 public class CPF {
+
+        private String numero;
+
+        public CPF(String numero) {
+                if (numero == null || !numero.matches("\\d{3}\\.\\d{3}\\.\\d{3}\\-\\d{2}")){
+                        throw new IllegalArgumentException("CPF Inválido!");
+                }
+                this.numero = numero;
+        }
 }
