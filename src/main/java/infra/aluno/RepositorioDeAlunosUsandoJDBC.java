e7f233ccc996baeac79b7f374c7fa5adfc2dd399
Thiago Machado <consultoria.tmachado@grupoivory.com.br>
2022-02-12 22:50:11 -0300

    amor da minha vida

--- src/main/java/infra/aluno/RepositorioDeAlunosUsandoJDBC.java
+++ src/main/java/infra/aluno/RepositorioDeAlunosUsandoJDBC.java
@@ -1,12 +1,10 @@
 package infra.aluno;
 
-import dominio.aluno.Aluno;
-import dominio.aluno.CPF;
-import dominio.aluno.RepositorioDeAlunos;
-import dominio.aluno.Telefone;
+import dominio.aluno.*;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
 
@@ -41,8 +39,33 @@ public class RepositorioDeAlunosUsandoJDBC implements RepositorioDeAlunos {
     }
 //result set
     @Override
-    public Aluno buscarPorCPF(CPF cpf) {
-        return null;
+    public Aluno buscarPorCPF(CPF cpf) throws SQLException {
+        {
+            String sql = "SELECT id, nome, email FROM ALUNO WHERE cpf = ?";
+            PreparedStatement ps = connection.prepareStatement(sql);
+            ps.setString(1, cpf.getNumero());
+
+            ResultSet rs = ps.executeQuery();
+            boolean encontrou = rs.next();
+            if (!encontrou) {
+                throw new IllegalArgumentException("Nao nao amigo");
+            }
+            String nome = rs.getNString("nome");
+            Email email = new Email(rs.getString("email"));
+            Aluno encontrado = new Aluno(cpf, nome, email);
+
+            Long id = rs.getLong("id");
+            sql = "SELECT ddd, numero FROM TELEFONE WHERE aluno_id = ?";
+            ps = connection.prepareStatement(sql);
+            ps.setLong(1, id);
+            rs = ps.executeQuery();
+            while (rs.next()) {
+                String numero = rs.getString("numero");
+                String ddd = rs.getString("ddd");
+                encontrado.adicionarTelefone(ddd, numero);
+            }
+            return encontrado;
+        }
     }
 
     @Override
